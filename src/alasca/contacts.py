f327dcce2ead655c91c73b7e3c04c76807326961
Eleftherios Mainas <emainas@dhcp-152-23-240-208.wireless-1x.unc.edu>
2025-04-22 10:57:35 -0400

    bug

--- src/alasca/contacts.py
+++ src/alasca/contacts.py
@@ -2,7 +2,8 @@
 import subprocess, sys
 
 def run_cpptraj(parmfile, trajfile):
-    cpp_input = f\"\"\"
+    """Run cpptraj to compute non-native contacts."""
+    cpp_input = f"""
 parm {parmfile}
 trajin {trajfile}
 nativecontacts name NC1 :170 :1-169 \\
@@ -10,33 +11,13 @@ nativecontacts name NC1 :170 :1-169 \\
     distance 3.0 \\
     skipnative
 go
-\"\"\"
-    p = subprocess.run(["cpptraj"], input=cpp_input,
-                       text=True, capture_output=True)
-    if p.returncode:
-        print(p.stderr, file=sys.stderr)
-        sys.exit(p.returncode)
-    print(p.stdout)
+"""
+    proc = subprocess.run(
+        ["cpptraj"], input=cpp_input,
+        text=True, capture_output=True
+    )
+    if proc.returncode != 0:
+        print(proc.stderr, file=sys.stderr)
+        sys.exit(proc.returncode)
+    print(proc.stdout)
 
-def post_process(dat, contacts_txt, frac_txt, resid_txt, thresh):
-    lines = [l for l in open(dat) if not l.startswith("#")]
-    open(contacts_txt, "w").writelines(lines)
-
-    filtered = [l for l in lines if float(l.split()[3]) >= thresh]
-    open(frac_txt, "w").writelines(filtered)
-
-    res = {l.split()[1].split("_:")[-1].split("@")[0] for l in filtered}
-    with open(resid_txt, "w") as f:
-        for r in sorted(res, key=int):
-            f.write(r + "\n")
-
-    print(f"Wrote {len(res)} residues to {resid_txt}")
-
-def run(parmfile, trajfile, frac_thresh=0.5,
-        contacts_txt="contacts2.txt",
-        frac_txt="fraction.txt",
-        resid_txt="2-resids.txt"):
-    \"\"\"Entry point for the `alasca contacts` subcommand.\"\"\"
-    run_cpptraj(parmfile, trajfile)
-    post_process("native-contacts.dat",
-                 contacts_txt, frac_txt, resid_txt, frac_thresh)
